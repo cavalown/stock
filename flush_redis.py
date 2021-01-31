7164aed6df8d842791db82ee609834e5fb78b982
cavalown <cavalown@gmail.com>
2021-01-31 22:03:17 +0800

    modify flush

--- flush_redis.py
+++ flush_redis.py
@@ -1,6 +1,14 @@
+import re
 from myPackage import redisServer as red
+import os
 
 
-connect = red.redis_connection('linode1', 'mongo', 0)
-kvs = red.flush_all_in_one_db(connect)
-print(len(kvs))
+def flush_redis():
+    connect = red.redis_connection('linode1', 'mongo', int(os.environ.get("db")))
+    kvs = red.flush_all_in_one_db(connect)
+    print(len(kvs))
+    return
+
+
+if __name__ == '__main__':
+    flush_redis()
