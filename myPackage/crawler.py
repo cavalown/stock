773d2bd02d8b59efe5cedf3c981c095cf0d88fef
cavalown <cavalown@gmail.com>
2021-03-02 22:44:15 +0800

    modify crawler exception file path

--- myPackage/crawler.py
+++ myPackage/crawler.py
@@ -93,7 +93,7 @@ def crawler(url):
             time.sleep(10)
             crawler(url)
         print(f'Exception: {e}')
-        wcsv.writeToCsv('./data/CrawlerException', [
+        wcsv.writeToCsv('../data/CrawlerException', [
             stock_id, year, month])
         documents = []
         return documents
